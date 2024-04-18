bf1b5f3e4d5aeeb7f39384ba8613084e4baef31a
AmrTaman <amrtaman4325@gmail.com>
2024-04-18 03:08:47 +0200

    d

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,6 @@ import uuid
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, DateTime, String
-from models.__init__ import storage
 
 
 Base = declarative_base()
@@ -18,6 +17,7 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
+            from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
