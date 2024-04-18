24d486218836580012ec921752770a3d4f1be520
AmrTaman <amrtaman4325@gmail.com>
2024-04-18 03:18:30 +0200

    d

--- models/state.py
+++ models/state.py
@@ -2,15 +2,21 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-from sqlalchemy.orm import Relationship
+from sqlalchemy.orm import relationship
+import os
 
 class State(BaseModel, Base):
-    """ State class """
+    """
+    ssssss
+    """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = Relationship("City", cascade="all, delete-orphan", backref='state')
-    @property
-    def cities(self):
-        from models import storage
-        from models.city import City
-        return [attr for attr in storage.all(City).values() if City.state_id == self.id]
+    
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship("City", cascade="all, delete", backref="state")
+    else:
+        @property
+        def cities(self):
+            from models import storage
+            from models.city import City
+            return [city for city in storage.all(City).values() if city.state_id == self.id]
