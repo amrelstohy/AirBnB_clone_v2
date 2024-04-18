45d1528b826dde5a4784f4f539b967b6e97510af
AmrTaman <amrtaman4325@gmail.com>
2024-04-18 02:03:35 +0200

    d

--- models/__init__.py
+++ models/__init__.py
@@ -10,7 +10,6 @@ if var == 'db':
     from models.state import State
     storage = DBStorage()
     storage.reload()
-    print(storage.all())
 else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
