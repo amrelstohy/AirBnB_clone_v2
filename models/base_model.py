ca361d1107f0c9155833520a02a11cc322ca40df
AmrTaman <amrtaman4325@gmail.com>
2024-04-18 02:28:37 +0200

    d

--- models/base_model.py
+++ models/base_model.py
@@ -37,7 +37,6 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
-        print(self.__dict__)
         storage.new(self)
         storage.save()
 
