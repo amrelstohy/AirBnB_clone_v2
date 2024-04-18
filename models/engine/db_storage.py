26763159b524ad03a20474f8f03c1322a1b5a976
AmrTaman <amrtaman4325@gmail.com>
2024-04-18 03:28:16 +0200

    d

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -66,7 +66,7 @@ class DBStorage():
         """
         Reload the db tables
         """
-        self.__session.close_all()
+        self.__session.close()
         Base.metadata.create_all(self.__engine)
 
 
