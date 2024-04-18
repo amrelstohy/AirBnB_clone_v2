c12728547c78bfdbe7d316ca7dba6b2a9e967f59
AmrTaman <amrtaman4325@gmail.com>
2024-04-18 02:48:08 +0200

    d

--- models/base_model.py
+++ models/base_model.py
@@ -35,10 +35,7 @@ class BaseModel:
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
-        from models import storage
-        self.updated_at = datetime.now()
-        storage.new(self)
-        storage.save()
+        print("Ss")
 
     def to_dict(self):
         """Convert instance into dict format"""
