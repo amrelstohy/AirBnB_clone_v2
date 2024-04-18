236b8cdef9e68b892b460ef71c20a1f7ff04f6ad
AmrTaman <amrtaman4325@gmail.com>
2024-04-18 18:35:26 +0200

    d

--- models/base_model.py
+++ models/base_model.py
@@ -17,7 +17,7 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
-            
+            from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
@@ -36,7 +36,7 @@ class BaseModel:
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
-        from models.__init__ import storage
+        from models import storage
         storage.new(self)
         storage.save()
 
