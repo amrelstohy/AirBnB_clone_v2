ae428053fa44f8273554ca25344884a0b3666acf
AmrTaman <amrtaman4325@gmail.com>
2024-04-19 02:40:49 +0200

    d

--- models/state.py
+++ models/state.py
@@ -12,11 +12,10 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
     
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship("City", cascade="all, delete", back_populates="state")
-    else:
-        @property
-        def cities(self):
-            from models import storage
-            from models.city import City
-            return [city for city in storage.all(City).values() if city.state_id == self.id]
+
+    cities = relationship("City", cascade="all, delete", back_populates="state")
+    @property
+    def cities(self):
+        from models import storage
+        from models.city import City
+        return [city for city in storage.all(City).values() if city.state_id == self.id]
