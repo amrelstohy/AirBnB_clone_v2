fa1d40c88199686a83c21af0f7515a8cab57b17c
amrelstohy <amramr2901@gmail.com>
2024-04-21 00:15:48 +0200

    hi

--- models/city.py
+++ models/city.py
@@ -12,5 +12,6 @@ class City(BaseModel, Base):
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         state = relationship("State", back_populates="cities")
+        places = relationship("Place", back_populates="cities")
 
 
