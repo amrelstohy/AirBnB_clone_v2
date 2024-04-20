d9676d93bcddcc2a43acc031bbb2415ff0088799
amrelstohy <amramr2901@gmail.com>
2024-04-21 00:21:27 +0200

    hiii

--- models/place.py
+++ models/place.py
@@ -4,7 +4,7 @@ from models.base_model import BaseModel
 from models.base_model import Base
 from sqlalchemy import Column, String, ForeignKey, Integer, Float
 
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = "places"
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
