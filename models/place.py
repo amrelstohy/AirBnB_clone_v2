d183260491d810e71030798d8748a325290de4f0
amrelstohy <amramr2901@gmail.com>
2024-04-21 04:59:10 +0200

    hiii

--- models/place.py
+++ models/place.py
@@ -6,20 +6,6 @@ from sqlalchemy import Column, Table, String, ForeignKey, Integer, Float
 from sqlalchemy.orm import relationship
 import os
 
-place_amenity = Table("place_amenity",
-            Base.metadata,
-            Column("place_id",
-                String(60),
-                ForeignKey("places.id"),
-                primary_key=True,
-                nullable=False),
-            Column("amenity_id",
-                String(60),
-                ForeignKey("amenities.id"),
-                primary_key=True,
-                nullable=False)
-            )
-
 
 class Place(BaseModel, Base):
     """ A place to stay """
@@ -40,6 +26,19 @@ class Place(BaseModel, Base):
         users = relationship("User", back_populates="places")
         cities = relationship("City", back_populates="places")
         reviews = relationship("Review", back_populates="place")
+        place_amenity = Table("place_amenity",
+            Base.metadata,
+            Column("place_id",
+                String(60),
+                ForeignKey("places.id"),
+                primary_key=True,
+                nullable=False),
+            Column("amenity_id",
+                String(60),
+                ForeignKey("amenities.id"),
+                primary_key=True,
+                nullable=False)
+            )
         amenities = relationship("Amenity", secondary="place_amenity", viewonly=False)
     else:
         @property
