bedd9d808d3f491a979f1ed252f29812cadc3f2e
amrelstohy <amramr2901@gmail.com>
2024-04-21 05:12:34 +0200

    hiii

--- models/amenity.py
+++ models/amenity.py
@@ -10,4 +10,6 @@ class Amenity(BaseModel, Base):
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        place_amenities = relationship("Amenity", secondary="place_amenity")
+        place_amenities = relationship('PlaceAmenity',
+            backref='amenities',
+            cascade='delete')
