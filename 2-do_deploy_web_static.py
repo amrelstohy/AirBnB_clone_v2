794130e12d19a1403221a12e7572fa2d2eaa315c
Amr Taman <96953623+AmrTaman@users.noreply.github.com>
2024-05-15 02:22:13 +0300

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,79 +1,60 @@
 #!/usr/bin/python3
-"""
-Deploying archive
-"""
-
-from fabric.api import put, run, env, local
-from datetime import datetime
+"""A module for web application deployment with Fabric."""
 import os
+from datetime import datetime
+from fabric.api import env, local, put, run, runs_once
 
-env.hosts = ['54.165.2.91', '54.210.108.204']
+
+env.hosts = ["34.73.0.174", "35.196.78.105"]
+"""The list of host server IP addresses."""
 
 
+@runs_once
 def do_pack():
-    """
-    sssssss
-    """
-    if not os.path.exists("versions"):
-        os.makedirs("versions")
-
-    now = datetime.now()
-    archive_name = "web_static_{}{}{}{}{}{}.tgz".format(
-        now.year, now.month, now.day, now.hour, now.minute, now.second)
-    result = local("tar -cvzf versions/{} web_static".format(
-        archive_name), capture=True)
-    if result.succeeded:
-        return (os.path.join("versions", archive_name))
-    else:
-        return (None)
+    """Archives the static files."""
+    if not os.path.isdir("versions"):
+        os.mkdir("versions")
+    cur_time = datetime.now()
+    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+        cur_time.year,
+        cur_time.month,
+        cur_time.day,
+        cur_time.hour,
+        cur_time.minute,
+        cur_time.second
+    )
+    try:
+        print("Packing web_static to {}".format(output))
+        local("tar -cvzf {} web_static".format(output))
+        archize_size = os.stat(output).st_size
+        print("web_static packed: {} -> {} Bytes".format(output, archize_size))
+    except Exception:
+        output = None
+    return output
 
 
 def do_deploy(archive_path):
+    """Deploys the static files to the host servers.
+    Args:
+        archive_path (str): The path to the archived static files.
     """
-    functiun of deployment
-    """
-
     if not os.path.exists(archive_path):
-        return (None)
+        return False
+    file_name = os.path.basename(archive_path)
+    folder_name = file_name.replace(".tgz", "")
+    folder_path = "/data/web_static/releases/{}/".format(folder_name)
+    success = False
     try:
-        put(archive_path, "/tmp/")
-        archive_name = archive_path.split('/')[-1]
-        folder_name = archive_name.split('.')[0]
-        if run("sudo mkdir -p /data/web_static/releases/{}".format(
-                folder_name)).failed is True:
-            return (False)
-        if run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(
-                archive_name, folder_name)).failed is True:
-            return (False)
-        if run("sudo rm /tmp/{}".format(archive_name)).failed is True:
-            return (False)
-        if run("sudo rm -rf /data/web_static/current").failed is True:
-            return (False)
-        if run("sudo mv /data/web_static/releases/{0}/web_static/*"
-                " /data/web_static/releases/{0}/".format(
-                    folder_name)).failed is True:
-            return (False)
-        if run("sudo rm -rf /data/web_static/releases/{}/web_static".format(
-                folder_name)).failed is True:
-            return (False)
-        if run("sudo ln -sf /data/web_static/releases/{}/"
-                " /data/web_static/current".format(
-                    folder_name)).failed is True:
-            return (False)
-        if run("sudo chown -R ubuntu:ubuntu /data/").failed is True:
-            return (False)
-        if run("sudo chmod -R 755 /data").failed is True:
-            return (False)
-        if run('new_loc="\\tlocation /hbnb_static {\\n\\t\\talias'
-                '/data/web_static/current;\\n\\t}"').failed is True:
-            return (False)
-        if run('sudo sed -i "/server_name _;/a\\ $new_loc"'
-                ' /etc/nginx/sites-enabled/default').failed is True:
-            return (False)
-        if run('sudo nginx -s reload').failed is True:
-            return (False)
-        if run('sudo service nginx restart').failed is True:
-            return (False)
-        return (True)
-    except Exception as e:
-        return (False)
+        put(archive_path, "/tmp/{}".format(file_name))
+        run("mkdir -p {}".format(folder_path))
+        run("tar -xzf /tmp/{} -C {}".format(file_name, folder_path))
+        run("rm -rf /tmp/{}".format(file_name))
+        run("mv {}web_static/* {}".format(folder_path, folder_path))
+        run("rm -rf {}web_static".format(folder_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(folder_path))
+        print('New version deployed!')
+        success = True
+    except Exception:
+        success = False
+    return success
