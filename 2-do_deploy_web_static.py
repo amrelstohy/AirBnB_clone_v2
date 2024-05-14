e9cecc994d6f2574655d1d26717978912cbe0cb3
Amr Taman <96953623+AmrTaman@users.noreply.github.com>
2024-05-15 00:35:02 +0300

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,10 +3,29 @@
 Deploying archive
 """
 
-from fabric.api import put, run, env
+from fabric.api import put, run, env, local
+from datetime import datetime
 import os
 
 env.hosts = ['54.165.2.91', '54.210.108.204']
+
+
+def do_pack():
+    """
+    sssssss
+    """
+    if not os.path.exists("versions"):
+        os.makedirs("versions")
+
+    now = datetime.now()
+    archive_name = "web_static_{}{}{}{}{}{}.tgz".format(
+        now.year, now.month, now.day, now.hour, now.minute, now.second)
+    result = local("tar -cvzf versions/{} web_static".format(
+        archive_name), capture=True)
+    if result.succeeded:
+        return (os.path.join("versions", archive_name))
+    else:
+        return (None)
 
 
 def do_deploy(archive_path):
