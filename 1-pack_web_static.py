6786c683bb5011fd26920f6c50b1cde7b02813cb
AmrTaman <amrtaman4325@gmail.com>
2024-05-14 01:57:07 +0000

    d

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,2 +1,27 @@
-#!/usr/bin/env bash
-#
+#!/usr/bin/python3
+"""
+this module is for compressing and deployment
+"""
+
+
+from fabric.api import local
+from datetime import datetime
+import os
+
+
+def do_pack():
+    """
+    sssssss
+    """
+    if not os.path.exists("versions"):
+        os.makedirs("versions")
+
+    now = datetime.now()
+    archive_name = "web_static_{}{}{}{}{}{}.tgz".format(
+        now.year, now.month, now.day, now.hour, now.minute, now.second)
+    result = local("tar -cvzf versions/{} web_static".format(
+        archive_name), capture=True)
+    if result.succeeded:
+        return (os.path.join("versions", archive_name))
+    else:
+        return (None)
