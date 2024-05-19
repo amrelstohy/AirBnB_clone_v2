6bbf9fa5d05279e8dde10321dd10b62ab8ba7407
Amr Taman <96953623+AmrTaman@users.noreply.github.com>
2024-05-19 20:48:46 +0300

    Update place.py

--- models/place.py
+++ models/place.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 """ holds class Place"""
+import models
 from models.base_model import BaseModel, Base
 from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
-from models import storage
 
-if models.storage == 'db':
+if models.storage_t == 'db':
     place_amenity = Table('place_amenity', Base.metadata,
                           Column('place_id', String(60),
                                  ForeignKey('places.id', onupdate='CASCADE',
@@ -21,8 +21,7 @@ if models.storage == 'db':
 
 class Place(BaseModel, Base):
     """Representation of Place """
-    from models import storage
-    if models.storage == 'db':
+    if models.storage_t == 'db':
         __tablename__ = 'places'
         city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
