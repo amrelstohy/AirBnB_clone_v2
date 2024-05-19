cf21b09c468f0a49bffcad8d2fbae2c9c4441f4d
Amr Taman <96953623+AmrTaman@users.noreply.github.com>
2024-05-19 20:52:38 +0300

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,18 +1,24 @@
-#!/usr/bin/python3
-""" Review module for the HBNB project """
-from models.base_model import BaseModel
-from models.base_model import Base
-from sqlalchemy import Column, String, ForeignKey, Integer, Float
-from sqlalchemy.orm import relationship
-import os
+#!/usr/bin/python
+""" holds class Review"""
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
 
 
 class Review(BaseModel, Base):
-    """ Review classto store review information """
-    __tablename__ = "reviews"
-    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
-    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
-    text = Column(String(1024), nullable=False)
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        user = relationship("User", back_populates="reviews")
-        place = relationship("Place", back_populates="reviews")
+    """Representation of Review """
+    if models.storage_t == 'db':
+        __tablename__ = 'reviews'
+        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        text = Column(String(1024), nullable=False)
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes Review"""
+        super().__init__(*args, **kwargs)
