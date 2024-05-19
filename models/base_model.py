84c4cfd38cb338eeca95394f0317cdd2f2fb4faa
Amr Taman <96953623+AmrTaman@users.noreply.github.com>
2024-05-19 20:49:59 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@ import uuid
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, DateTime, String
+import models
 
 if models.storage_t == "db":
     Base = declarative_base()
