10a6d16789123012a793878b029f003531032ec1
Amr Taman <96953623+AmrTaman@users.noreply.github.com>
2024-05-19 22:52:20 +0300

    Update place.py

--- models/place.py
+++ models/place.py
@@ -9,11 +9,11 @@ from sqlalchemy.orm import relationship
 
 if models.storage_t == 'db':
     place_amenity = Table('place_amenity', Base.metadata,
-                          Column('place_id', VARCHAR(60),
+                          Column('place_id', String(60),
                                  ForeignKey('places.id', onupdate='CASCADE',
                                             ondelete='CASCADE'),
                                  primary_key=True),
-                          Column('amenity_id', VARCHAR(60),
+                          Column('amenity_id', String(60),
                                  ForeignKey('amenities.id', onupdate='CASCADE',
                                             ondelete='CASCADE'),
                                  primary_key=True))
@@ -23,10 +23,10 @@ class Place(BaseModel, Base):
     """Representation of Place """
     if models.storage_t == 'db':
         __tablename__ = 'places'
-        city_id = Column(VARCHAR(60), ForeignKey('cities.id'), nullable=False)
-        user_id = Column(VARCHAR(60), ForeignKey('users.id'), nullable=False)
-        name = Column(VARCHAR(128), nullable=False)
-        description = Column(VARCHAR(1024), nullable=True)
+        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024), nullable=True)
         number_rooms = Column(Integer, nullable=False, default=0)
         number_bathrooms = Column(Integer, nullable=False, default=0)
         max_guest = Column(Integer, nullable=False, default=0)
