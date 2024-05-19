ea5b0164d301782e43f87a313edb4c4ca0ecafe1
Amr Taman <96953623+AmrTaman@users.noreply.github.com>
2024-05-19 23:12:10 +0300

    Update place.py

--- models/place.py
+++ models/place.py
@@ -23,8 +23,6 @@ class Place(BaseModel, Base):
     """Representation of Place """
     if models.storage_t == 'db':
         __tablename__ = 'places'
-        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
         name = Column(String(128), nullable=False)
         description = Column(String(1024), nullable=True)
         number_rooms = Column(Integer, nullable=False, default=0)
@@ -37,6 +35,8 @@ class Place(BaseModel, Base):
         amenities = relationship("Amenity", secondary="place_amenity",
                                  backref="place_amenities",
                                  viewonly=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
     else:
         city_id = ""
         user_id = ""
