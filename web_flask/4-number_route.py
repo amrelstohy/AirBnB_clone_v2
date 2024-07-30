915b2f8b752ec4f49328d6588c6dd7bc582c8d03
Amr Elstohy <amramr2901@gmail.com>
2024-07-30 18:04:32 +0000

    hi

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -34,6 +34,7 @@ def python(text):
     """ displays python and the text """
     return "Python " + text.replace('_', ' ')
 
+
 @app.route("/number/<int:n>", strict_slashes=False)
 def number(n):
     """ displays n if is a number """
