eee4332d352d53a18d78534a127b0c5416e4a732
Amr Elstohy <amramr2901@gmail.com>
2024-07-30 19:53:00 +0000

    hi

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -39,7 +39,7 @@ def python(text):
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def temlate(n):
     """ displays html page """
-    return render_template('5-number.html', num = n)
+    return render_template('5-number.html', num=n)
 
 
 @app.route("/number/<int:n>", strict_slashes=False)
